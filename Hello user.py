35d04ab3defcaef6747722c12b67b85ed3548870
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-19 14:30:12 +0530

    Added Hello User

---
+++ Hello user.py
@@ -0,0 +1,9 @@
+#Write a program which does the following
+
+#Declare a string variable x
+#Accept a text user input - the name of the user - and store it in the variable x
+#Output and print to the console "Hello" before the user defined name.
+#Remember to add a space between "Hello" and x
+
+x= str(input())
+print("Hello "+x)
