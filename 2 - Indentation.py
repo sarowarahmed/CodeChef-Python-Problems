9e4ca7901ec7369cb112a007ceaa37d7ef03a05f
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-20 23:22:38 +0530

    Added 2 - Indentation

---
+++ 2 - Indentation.py
@@ -0,0 +1,19 @@
+#r = 1000
+#w = 3222
+#if r > w:
+#print("White balls are out of stock")  
+#else:
+#print("Your order is Confirmed")
+
+#This code will not run due to improper indentation
+#Python relies on indentation (whitespace at the beginning of a line) to define scope in the code.
+#Other programming languages often use curly brackets for this purpose.
+#Fix the error by putting a space before both print
+
+r = 1000
+w = 3222
+if r > w:
+    print("White balls are out of stock")  
+else:
+    print("Your order is Confirmed")
+
