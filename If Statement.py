8875ba7738b27b4d169d8c6737ac77fba0ef56e9
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-21 23:38:12 +0530

    Added If Statement

---
+++ If Statement.py
@@ -0,0 +1,9 @@
+#Write a program which does the following
+#Take input for two integer variables a & b
+#Output "Coding is Fun!" to the console if a is greater than b
+#If a is less than or equal to b - output nothing
+
+a=int(input())
+b=int(input())
+if a>b:
+    print("Coding is Fun!")
