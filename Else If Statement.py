b59446afdca1102ba03511c3be8b6d0efb0e699b
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-22 23:55:36 +0530

    Added Else If Statement

---
+++ Else If Statement.py
@@ -0,0 +1,15 @@
+# Update the code below to solve Task-1
+b = 23
+r = 45
+if r > b:
+    print("Rob Scored higher marks than Bob")
+elif r == b:
+    print("Bob & Rob both scored the same")
+
+# Update the code below to solve Task-2
+b = 15
+r = 15
+if r > b:
+    print("Rob Scored higher marks than Bob")
+elif r == b:
+    print("Bob & Rob both scored the same")
