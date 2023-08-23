78e1b2cbe02ae60af2fd759297e54561db229692
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-23 23:24:50 +0530

    Added Else Statement

---
+++ Else Statement.py
@@ -0,0 +1,33 @@
+#Create integer variables r and k - the weight of friends Ram and Karan
+#Initialise r and k as r = 24 and k = 32
+
+#Compute and output the following to the console
+#"Ram is heavier than Karan" if r is greater than k
+#"Karan is heavier than Ram" if r is lesser than k
+#"Ram & Karan have the same weight!" for any remaining conditions.
+
+#Update the values of r and k as r = 78 and k = 78
+#Compute and output the following to the console
+#"Ram is heavier than Karan" if r is greater than k
+#"Karan is heavier than Ram" if r is lesser than k
+#"Ram & Karan have the same weight!" for any remaining conditions.
+
+r = 24
+k = 32
+
+if r> k:
+    print("Ram is heavier than Karan")
+elif r< k:
+    print("Karan is heavier than Ram")
+else:
+    print("Ram & Karan have the same weight!")
+    
+r = 78
+k = 78
+
+if r> k:
+    print("Ram is heavier than Karan")
+elif r<k:
+    print("Karan is heavier than Ram")
+else:
+    print("Ram & Karan have the same weight!")
