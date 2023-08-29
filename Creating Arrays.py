70c813759e5f40d3f58edac944a0000d6333b58c
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-29 23:04:27 +0530

    Added Creating Arrays

---
+++ Creating Arrays.py
@@ -0,0 +1,8 @@
+#Write a program which does the following
+
+#Create an Array of the first 5 positive integers - name it 'integers'
+#Once the array is define - output "Done" to the console.
+
+integers = [31,45,77,53,95]
+
+print("Done")
