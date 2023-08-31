b23086a057add3bf805900be695ffeedeee91a86
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-31 23:30:10 +0530

    Added Changing the elements of an Array

---
+++ Changing the elements of an Array.py
@@ -0,0 +1,9 @@
+#Write a program which does the following
+
+#The 3rd month in the given list is incorrect
+#Update the 3rd month in the given array with the correct one - "Mar"
+#Once the 3rd array element is updated, output the entire list to the console
+
+months = ["Jan", "Feb", "Dec", "Apr"]
+months[2]="Mar"
+print(months)
