17575ebdcd4bec77db7095e50ff13e713a589f74
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-01 22:37:01 +0530

    Added Displaying elements

---
+++ Displaying elements.py
@@ -0,0 +1,9 @@
+#Write a program which does the following
+
+#Create a string array for the following values "Monday", "Tuesday", "Wednesday", and "Thursday".
+#Name the list as per your choice
+#Output the last two elements of the array on separate lines
+
+lists = ["Monday", "Tuesday", "Wednesday","Thursday"]
+print(lists[-1])
+print(lists[-2])
