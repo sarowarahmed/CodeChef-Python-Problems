c485d41925f3cbd83b5a0f141b8ce898109c0df2
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-02 23:50:24 +0530

    Added Displaying the count of elements

---
+++ Displaying the count of elements.py
@@ -0,0 +1,7 @@
+#Write a program which does the following
+
+#Create an int array containing the elements - 10, 20, 30, 40, 50, 60
+#Compile and output to the console the accurate count of the number of integer elements in the given array.
+
+myNumbers = [10, 20, 30, 40, 50, 60]
+print(len(myNumbers)) 
