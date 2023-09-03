6163c17f50c31a4190cb2d5a14d78b759afa76f8
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-03 23:46:21 +0530

    Added 2 - Negative Indexing

---
+++ 2 - Negative Indexing.py
@@ -0,0 +1,8 @@
+#Write a program which does the following:
+
+#Create a list containing the elements - "lock", "key", "barrier", "sign" and "door"
+#Print the 2nd last element from the given list.
+#i.e. - reading from right to left, print the 2nd element
+
+list = ["lock","key","barrier","sign","door"]
+print(list[-2])
