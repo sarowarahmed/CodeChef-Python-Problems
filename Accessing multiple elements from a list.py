86ca7b0ff9e077ea87b7a8274e6f12bf0f168f2c
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-04 23:40:50 +0530

    Added Accessing multiple elements from a list

---
+++ Accessing multiple elements from a list.py
@@ -0,0 +1,11 @@
+#Write a program which does the following:
+
+#Create a list months of the first 7 months in a year
+#Output the following to the console on separate lines
+#Print the first 6 months of the year
+#Print the 2nd to 5th month of the year - both 2nd and 5th included
+
+months = ["January", "February", "March", "April", "May", "June", "July"]
+print(months[0:6])
+
+print(months[1:5])
