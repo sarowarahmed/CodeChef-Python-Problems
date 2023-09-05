e6bd4d99e544ae5c17e0be6618c5105da52514db
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-05 22:56:31 +0530

    Added While Loop

---
+++ While Loop.py
@@ -0,0 +1,11 @@
+#Write a program which does the following
+
+#Declare a variable 'a' and initialise it to 0
+#Use the syntax above to create a loop, output the following to the console
+#Print 'a' in separate lines as long as it is less than 7.
+#Increment a by 1 in each iteration.
+
+a = 0
+while a< 7:
+    print(a)
+    a += 1
