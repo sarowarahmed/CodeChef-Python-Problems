0a81130cae4737e81b5eceaf999aa43bfcde4cee
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-06 23:48:17 +0530

    Added User Input Loop

---
+++ User input Loop.py
@@ -0,0 +1,14 @@
+#Write a program which does the following
+
+#Declare a variable num and store a user defined input from the console in it
+#Declare a variable a and initialise it to 0
+#Use the while syntax to create a loop, output the following to the console
+#Print a in separate lines as long as it is less than num.
+#Increment a by 1 in each iteration.
+
+num = int(input())
+a = 0
+
+while a < num:
+    print(a)
+    a = a + 1
