a431f45841ec29682e5bd15026cd961e8fab7119
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-07 23:36:48 +0530

    Added For Loop

---
+++ For Loop.py
@@ -0,0 +1,8 @@
+#Write a program which does the following
+
+#You are given a list of numbers
+#Print each item of the list on a separate line
+
+numbers = [1, 6, 4, 3, 2, 5]
+for item in numbers:
+    print(item)
