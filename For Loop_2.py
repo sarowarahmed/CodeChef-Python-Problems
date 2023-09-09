091beea20975b346f2b7e896982c204c0612a611
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-09-09 23:31:40 +0530

    Added For Loop_2

---
+++ For Loop_2.py
@@ -0,0 +1,8 @@
+#Write a program which does the following
+
+#Accepts a user-defined input as the variable num 
+#Output all positive integers from 1 till num - num inclusive - on a separate line
+
+num=int(input())
+for i in range(1,num+1):
+    print (i)
